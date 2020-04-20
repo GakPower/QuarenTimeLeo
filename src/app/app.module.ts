c10e763b0c9a90e87f0583bf8af87758583d6bba
GakPower <alanigogos@gmail.com>
2020-04-20 16:09:31 +0200

    Added scss variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { PictureCardComponent } from './Components/Secondary/picture-card/picture-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureCardComponent
   ],
   imports: [
     BrowserModule,
