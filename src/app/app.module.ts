fc070b00feccd60f2fc368e699f1e3147eddc8e2
Osman osman <Oosman@kth.se>
2020-04-21 10:49:33 +0200

    solve some issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,13 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { PictureCardComponent } from './Components/Secondary/picture-card/picture-card.component';
 import { WatchItComponent } from './Components/Secondary/watch-it/watch-it.component';
-import { TopTenItemComponent } from './Components/Secondary/topTen-item/top-ten-item/top-ten-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PictureCardComponent,
     WatchItComponent,
-    TopTenItemComponent
+    
   ],
   imports: [
     BrowserModule,
