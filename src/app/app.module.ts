7dae3fd75ebf626e641002d73baa9fb54030c27b
Lorenzo Bracci <lorenzobracci98@hotmail.com>
2020-04-22 08:18:41 +0200

    Registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,23 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { PictureCardComponent } from './Components/Secondary/picture-card/picture-card.component';
 import { WatchItComponent } from './Components/Secondary/watch-it/watch-it.component';
+import { RegistrationComponent } from './Components/Secondary/registration/registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';//imported for filling forms
 
 @NgModule({
   declarations: [
     AppComponent,
     PictureCardComponent,
-    WatchItComponent
+    WatchItComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule,
+     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
