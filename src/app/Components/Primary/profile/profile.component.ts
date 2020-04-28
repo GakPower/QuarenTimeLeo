551c977250f55b1cb4268c54030544b8e875b137
Osman osman <Oosman@kth.se>
2020-04-28 08:58:17 +0200

    commiting unsaved changes

--- src/app/Components/Primary/profile/profile.component.ts
+++ src/app/Components/Primary/profile/profile.component.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import {WatchMeMovies  } from '../../Secondary/my-lists/myLists'; 
-import {AvatarList} from '../../Secondary/my-lists/myLists'; 
-
-=======
 import { Component, } from '@angular/core';
 import {Movie} from '../../Class/Movie/movie';
->>>>>>> 43f521ef31b9f3accd15a8b56599114484145365
 
 @Component({
   selector: 'app-profile',
@@ -20,17 +13,12 @@ export class ProfileComponent {
     avatar: 'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm208batch7-adj-09.png?w=1000&dpr=1&fit=default&crop=default&auto=null&fm=png&q=75&vib=3&con=3&usm=15&bg=transparent&ixlib=js-2.2.1&s=bdad6d5b0b529765d41000ef8cffcaad',
     email: 'test@gmail.com'
   };
-  Avatars = AvatarList; 
-  WatchList = WatchMeMovies;  
+   
   topics = [
     {title: 'Watch List', color: '#FFC857'}, {title: 'Favourite', color: '#E9724C'} ,{title: 'Black List', color: '#C5283D'} , {title: 'Watched', color: '#255f85'}
   ];
 
-<<<<<<< HEAD
-  
-=======
   WatchList: Movie[] = [];
->>>>>>> 43f521ef31b9f3accd15a8b56599114484145365
   cards = ['15661', '65161', '78913'];
   selectedTopic = -1;
 
--- src/app/Components/Secondary/my-lists-movie/my-lists-movie.component.ts
+++ src/app/Components/Secondary/my-lists-movie/my-lists-movie.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { MoviePageComponent } from '../movie-page/movie-page.component';
-<<<<<<< HEAD
-import { WatchedMovies} from '../my-lists/myLists'; 
-=======
 import {Movie} from '../../Class/Movie/movie';
->>>>>>> 43f521ef31b9f3accd15a8b56599114484145365
 
 @Component({
   selector: 'app-my-lists-movie',
@@ -14,14 +10,9 @@ import {Movie} from '../../Class/Movie/movie';
   styleUrls: ['./my-lists-movie.component.scss']
 })
 export class MyListsMovieComponent implements OnInit {
-<<<<<<< HEAD
   @Input() Option1: boolean; 
   @Input() WatchedList: Movie[]; 
-  watchedMovies = WatchedMovies; 
-=======
-  @Input() Option1: boolean;
-  @Input() WatchedList: Movie[];
->>>>>>> 43f521ef31b9f3accd15a8b56599114484145365
+ 
 
   constructor(public dialog: MatDialog) { }
 
@@ -34,7 +25,6 @@ export class MyListsMovieComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<any[]>){
-<<<<<<< HEAD
 
 
     if(event.previousContainer.id !== event.previousContainer.id){
@@ -45,12 +35,6 @@ export class MyListsMovieComponent implements OnInit {
       moveItemInArray(this.WatchedList, event.previousIndex, event.currentIndex);
       console.log(event) 
 
-=======
-    if (event.previousContainer.id !== event.previousContainer.id){
-      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      moveItemInArray(this.WatchedList, event.previousIndex, event.currentIndex);
->>>>>>> 43f521ef31b9f3accd15a8b56599114484145365
     }
   }
 }
