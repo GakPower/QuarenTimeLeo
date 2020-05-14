f2bbe71b58950b5de4cf8d02d27f1a50c52ec099
testgit <testemail>
2020-05-14 17:02:59 +0200

    bunch of stuff

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ let fs = require('fs');
 let all = fs.readFileSync('C://Users//anasa//Documents//Network and Communication//task1//FinalMovieDataset.txt', "utf8");
 let router = express.Router();
 
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://localhost:27017/test');
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("success");
 });
 
-
+let ObjectID = require('mongodb').ObjectID;
 
 app.use(cors());
 var Schema = mongoose.Schema;
@@ -25,16 +25,32 @@ var Schema = mongoose.Schema;
 var userRatings = new Schema({ 
     email: String,
     average: Number,
-    ratings: [{movieId: Number, rate: Number}]
-
+    ratings: [{movieId: Number, rate: Number}],
+    newratings: Number
 });
 
 var resources = new Schema( { 
+    MoviesLength:Number,
+    UsersLength:Number,
     Type: String,
     Dataset: []
 });
 
-
+var newMovies = new Schema( {
+    movieId: Number,
+    counter: Number,
+    ratings:[{email:String,rate:Number}]
+})
+var userData = new Schema({ //START OF NEW CODE
+    email: String,
+    icon: String,
+    topic: [{title: String, color: String, movieIDs: []}],
+    reccomendations: [],
+    takenSurvey: Boolean
+}); //END OF NEW CODE
+    
+var UserData = mongoose.model('UserData', userData);
+var NewMovies = mongoose.model('NewMovies',newMovies);
 var Recources = mongoose.model('Recources', resources);
 var UserRatings = mongoose.model('UserRatings', userRatings);
 
@@ -78,10 +94,11 @@ app.get('/user', function(req,res,next) {
 
 app.post('/user-add', function(req,res) {
     console.log(req.body);
-     let user =  new UserRatings({email: req.body.email, average: req.body.average, ratings: req.body.ratings});
-    
-    db.collection("UserRatings").insertOne(user, function (err, results) {  
-                                    //how to insert users into mongodb
+     let user =  new UserRatings({email: req.body.email, average: req.body.average, ratings: req.body.ratings,newratings: 0});
+     db.collection("Recources").findOne({Type: 'movies'}).then(elem => {
+        db.collection("Recources").updateOne({Type:'movies'}, {$set:  {UsersLength: elem.UsersLength + 1}});
+    })
+    db.collection("UserRatings").insertOne(user, function (err, results) {                                                                                                                           //how to insert users into mongodb
             res.json(results);
     });
 })
@@ -89,20 +106,318 @@ app.post('/user-add', function(req,res) {
 
 
 app.get('/translation', function(req,res,next) { 
-    db.collection("Recources").find().then(res => { 
-        res.forEach(elem => { 
-            console.log(elem.Dataset[0]); });
-    })
+    db.collection("Recources").findOne({Type: 'movies'}).then(result => { 
+            res.send(result.Dataset); });
 })
 
 
-db.collection("UserRatings").find().then(doc => { 
-    doc.forEach(elem => { 
+app.get('/get-length', function(req,res){ 
+    db.collection("Recources").findOne({Type:'movies'}).then(result=> {
+        var lengths = [result.MoviesLength, result.UsersLength];
+        res.send(lengths);
+    })
+})
+
+app.post('/rate-movie', function(req,res,next) { 
+    let rating= req.body.rating;
+    let useremail = req.body.email;
+    let usermovie = req.body.movie;
+    db.collection("UserData").updateOne(//erase the movie that has been rated from the reccomendations (if it isn t there nothing will happen)
+      { email: useremail},//find our current user
+      { $pull:  { reccomendations: Number(usermovie) } },//remove the movie that the user rated from the reccomendations
+      { multi: false });
+
+    
+
+    db.collection("Recources").findOne({Type: 'movies'}).then(doc=>{
+        var inDataset = false; 
+        
+            if(isIncluded(doc.Dataset,usermovie)) {
+          
+               
+                inDataset=true;
+                let done = false;
+                db.collection("UserRatings").findOne({email: useremail}).then(userdoc => { 
+                    let length = userdoc.ratings.length;
+                    let avg = userdoc.average;
+                    let ratingsCounter = userdoc.newratings;
+                    
+                    userdoc.ratings.forEach( userratings=> {
+                        if(userratings.movieId === doc.Dataset.indexOf(Number(usermovie))){
+                            done = true;
+                            
+                            let sum = ((avg*length) - userratings.rate) + Number(rating);
+                            let newavg = sum/length;
+                            
+                            
+                            db.collection("UserRatings").updateOne({email: useremail},{$set:{average: newavg}});
+                            db.collection("UserRatings").updateOne({email: useremail,"ratings.movieId": doc.Dataset.indexOf(Number(usermovie))},{$set:{"ratings.$.rate": Number(rating)}});
+                            
+                            if(ratingsCounter > 9){ 
+                                db.collection("UserRatings").updateOne({email:useremail},{$set:{newratings:0}});
+                            }
+                                res.json(JSON.stringify(ratingsCounter));
+                        }    
+                    })
+                    
+                    if(!done) {
+                        let sum2 = (avg*length) + Number(rating);
+                        let movierating = {movieId: doc.Dataset.indexOf(Number(usermovie)), rate: Number(rating)}
+                        let newavg2 = sum2/(length+1);
+                       
+                        db.collection("UserRatings").updateOne({email: useremail},{$set:{average: newavg2}});
+                                                                                                                  //dataset=translation
+                        db.collection("UserRatings").updateOne({email: useremail},{$push: {ratings: movierating}});     
+                        db.collection("UserRatings").updateOne({email: useremail},{$inc: {newratings: 1}} );
+                        let currentratings = ratingsCounter+1;
+                      
+                        if(currentratings > 9){
+                            db.collection("UserRatings").updateOne({email:useremail},{$set:{newratings:0}});
+                        }
+                        res.json(JSON.stringify(currentratings));              
+                    }
+                })
+               
+            }
+
+            if(!inDataset) { 
+               db.collection("NewMovies").findOne({movieId: Number(usermovie)}).then((doc) => {
+                   if(doc == null){
+                       
+                        let newmovie  =  new NewMovies({movieId: usermovie, counter: 1, ratings:[{email:useremail,rate:rating}]});
+                        db.collection("NewMovies").insertOne(newmovie, function(err,results) {});
+                        res.json("0");
+                   }
+                   else { 
+                       if(doc.counter == 5) {
+                            db.collection("NewMovies").remove({movieId: doc.movieId});
+                            db.collection("Recources").findOne({Type: 'movies'}).then(elem => { 
+                                var newID = elem.MoviesLength;
+                                let userNewRatings = 0;
+                                db.collection("UserRatings").findOne({email:useremail}).then(userelem => { 
+                                    let sum = (userelem.average*userelem.ratings.length) + Number(rating);
+                                    let newavg = sum/(userelem.ratings.length+1);
+                                    userNewRatings = userelem.newratings+1;
+
+                                    db.collection("UserRatings").updateOne({email: useremail},{$push: {ratings: {movieId:newID, rate: Number(rating)}} });
+                                    db.collection("UserRatings").updateOne({email: useremail},{$set: {average:newavg} });
+                                    db.collection("UserRatings").updateOne({email: useremail},{$inc:{newratings: 1}});
+                                 
+                                })
+                                    
+                                
+                                for(let i = 0; i < doc.ratings.length; i++){
+                                    db.collection("UserRatings").findOne({email: doc.ratings[i].email}).then(result => {
+                                        console.log(result.ratings); 
+                                        let sum = ((result.average)*result.ratings.length) + doc.ratings[i].rate;
+                                        let newavg = sum/(result.ratings.length + 1);
+                                        
+                                        db.collection("UserRatings").updateOne({email: doc.ratings[i].email},{$set:{average:newavg}});
+                                        db.collection("UserRatings").updateOne({email: doc.ratings[i].email},{$push: {ratings: {movieId:newID, rate: doc.ratings[i].rate}} });
+                                        db.collection("UserRatings").updateOne({email: doc.ratings[i].email},{$inc:{newratings: 1}});
+                                    })    
+                                   
+                                }    
+                            
+                                db.collection("Recources").updateOne({Type:'movies'}, {$set:  {MoviesLength: elem.MoviesLength + 1}});
+                                db.collection("Recources").updateOne({Type:'movies'},{$push: {Dataset: Number(usermovie)}});
+                                if(userNewRatings > 9){
+                                    db.collection("UserRatings").updateOne({email:useremail},{$set:{newratings:0}});
+                                }
+                                    res.json(JSON.stringify(userNewRatings));
+                            })
+                            
+                       }
+                       else {
+                           let found = false;
+                           for(let indx = 0; indx < doc.ratings.length;indx++){
+                               if(doc.ratings[indx].email === useremail) {
+                                   console.log("user found");
+                                    found = true;
+                                    db.collection("NewMovies").updateOne({movieId: Number(usermovie),"ratings.email": useremail},{$set:{"ratings.$.rate": Number(rating)}});
+                                    break;
+                               }
+                           }
+                           if(!found) {
+                                db.collection("NewMovies").updateOne({movieId: Number(usermovie)},{$set:{counter:doc.counter + 1}} );
+                                db.collection("NewMovies").updateOne({movieId: Number(usermovie)},{$push:{ratings:{email:useremail, rate:Number(rating)}} });
+                           }
+                           res.json("0");
+                       }
+                   }
+               })
+            }
+        
+    })
+  
+
+})
+
+function isIncluded(array, element){
+    return array.some(e => e == element);
+}
+
+
+app.get('/init-user', function(req, res, next){ //HERE I AM WRITING CODE(TO INITIALIZE AN USER)
+    let user =  new UserData({email: req.query.email, icon: '0x1F606', topic: [{title: 'Watch List', color: '#FFC857', movieIDs: []}, {title: 'Favourite', color: '#E9724C', movieIDs: []},
+    {title: 'Black List', color: '#C5283D', movieIDs: []}, {title: 'Watched', color: '#255F85', movieIDs: []}], reccomendations: [], takenSurvey: false});
+
+    db.collection("UserData").insertOne(user, function (err, result) {
+    if (err){
+        res.sendStatus(400);//send error message BAD REQUEST
+    } 
+    else {
+        res.sendStatus(200);//send OK message
+    }
+    });
+}); //HERE I FINISH WRITING CODE
+
+
+app.post('/store-recommendation', function(req, res) {//MORE NEW CODE(TO STORE ALL THE RECOMMENDED MOVIES)
+    db.collection("UserData").updateOne({email: req.body.email}, {$set:  {reccomendations: req.body.ratings,takenSurvey: true}}, function (err, result) {
+    if (err){
+        res.sendStatus(400);//send error message BAD REQUEST
+    }   
+    else {
+        
+        res.sendStatus(200);//send OK message
+    }
+    });
+});//HERE I FINISH TO WRITE MORE NEW CODE
+    
+app.get('/add-topic', function(req, res, next){ //HERE I AM WRITING CODE TO ADD A NEW TOPIC(PRE-DEFINED FORMAT)
+    db.collection("UserData").updateOne({email: req.query.email}, {$push: {topic: {title: 'My List', color: '#9ed964', movieIDs: [], _id:new ObjectID()} }}, function (err, result) {
+    if (err){
+        res.sendStatus(400);//send error message BAD REQUEST
+    }
+    else {
+        res.sendStatus(200);//send OK message
+    }
+    });
+}); //HERE I FINISH WRITING CODE
+    
+
+app.get('/delete-topic', function(req, res, next){ //HERE I AM WRITING CODE TO DELETE A TOPIC
+      db.collection("UserData").findOne({email: req.query.email}).then(doc=>{
+        let id = doc.topic[req.query.index]._id;//get the id of the correct element in the array
+      db.collection("UserData").updateOne(
+        { email: doc.email},//find our current user
+        { $pull:  { topic: {  _id: id } } },//remove the topic with the correct id(with found the correct id given the idex when we initialize the variable id)
+        { multi: false }, //this means that we only want to remove one element
+        function (err, result) {
+      if (err){
+        res.sendStatus(400);//send error message BAD REQUEST
+      }else{
+          res.sendStatus(200);//send OK message
+      }
+        })
+      });
+    }); //HERE I FINISH WRITING CODE
+    
+    app.get('/load-list', function(req, res, next){//NEW CODE TO LOAD ALL THE USER DATA FOR A SPECIFIED EMAIL
+      db.collection("UserData").findOne({email: req.query.email}).then(doc=>{
+        res.json(doc);//send json file back to client
+      });
+    });// HERE I FINISH WRITING NEW CODE
+    
+    app.get('/update-icon', function(req, res) {//MORE NEW CODE TO UPDATE AN ICON
+      db.collection("UserData").updateOne({email: req.query.email}, {$set:  {icon: req.query.icon}}, function (err, result) {//UPDATE ICON
+    if (err){
+      res.sendStatus(400);//send error message BAD REQUEST
+    }else{
+        res.sendStatus(200);//send OK message
+    }
+      });
+    });//HERE I FINISH TO WRITE MORE NEW CODE
+    
+    app.get('/delete-movie-from-topic', function(req, res, next){ //HERE I AM WRITING CODE TO A MOVIE FROM A TOPIC(I NEED THE EMAIL, THE TOPIC INDEX, THE MOVIEID AND THEN I CAN REMOVE THE MOVIE FROM THE LIST)
+        db.collection("UserData").findOne({email: req.query.email}).then(doc=>{
+          let id = doc.topic[req.query.index]._id;//get the id of the correct element in the array
+        db.collection("UserData").updateOne(
+          { email: doc.email, "topic._id": id},//find our current user
+          { $pull:  { "topic.$.movieIDs": Number(req.query.movieID) } },//remove the topic with the correct id(with found the correct id given the idex when we initialize the variable id)
+          { multi: false }, //this means that we only want to remove one element
+          function (err, result) {
+        if (err){
+          res.sendStatus(400);//send error message BAD REQUEST
+        }else{
+            res.sendStatus(200);//send OK message
+        }
+          })
+        });
+      }); //HERE I FINISH WRITING CODE
+      //db.collection("UserRatings").update({email: useremail},{$push: {ratings: movierating}});
+      app.get('/add-movie-to-topic', function(req, res, next){//HERE I WRITE CODE TO ADD A NEW MOVIE TO AN EXISTING TOPIC GIVEN THE INDEX, THE MOVIEID AND THE EMAIL
+        db.collection("UserData").findOne({email: req.query.email}).then(doc=>{
+          let id = doc.topic[req.query.index]._id;//get the id of the correct element in the array
+        db.collection("UserData").updateOne( { email: doc.email, "topic._id": id}, { $push:  { "topic.$.movieIDs": Number(req.query.movieID) } },
+          function (err, result) {
+        if (err){
+          res.sendStatus(400);//send error message BAD REQUEST
+        }else{
+            res.sendStatus(200);//send OK message
+        }
+          })
+        });
+      });// HERE I FINISH WRITING CODE
+
+      app.get('/update-topic', function(req, res, next){
+        db.collection("UserData").findOne({email: req.query.email}).then(doc=>{
+          let id = doc.topic[req.query.index]._id;//get the id of the correct element in the array
+          console.log(req.query.color);
+          console.log(req.query.title);
+        db.collection("UserData").updateOne( { email: doc.email, "topic._id": id}, { $set:  { "topic.$.title": req.query.title,  "topic.$.color": req.query.color} },
+          function (err, result) {
+        if (err){
+          res.sendStatus(400);//send error message BAD REQUEST
+        }else{
+            res.sendStatus(200);//send OK message
+        }
+          })
+        });
+        });
+
+
+
+/*db.collection("UserRatings").remove({email:'999'});
+db.collection("UserRatings").remove({email:'888'});
+db.collection("UserRatings").remove({email:'777'});
+db.collection("UserRatings").remove({email:'1111'});
+db.collection("UserRatings").remove({email:'3333'});
+db.collection("UserRatings").remove({email:'4444'});*/
+let testUser = new UserRatings({email: '999', average: 3, ratings: [{movieId: 30, rate: 3}], newratings: 0});
+//let newmovie2  =  new NewMovies({movieId: 32, counter: 1, ratings:[{email:44,rate:5}]});                                                                                                   //TESTING PURPOSES
+//db.collection("NewMovies").insertOne(newmovie2, function(err,results) {});
+    
+//db.collection("UserRatings").insert(testUser,function(err,res){});
+//db.collection("UserRatings").update({email:'999',"ratings.movieId": 23},{$inc:{"ratings.$.rate": 1}});
+
+
+//db.collection("Recources").update({Type:'movies'}, {$set: {MoviesLength:9742}});
+//db.collection("Recources").update({Type:'movies'}, {$set: {UsersLength:610}});
+//db.collection("Recources").update({Type:'movies'},{$push: {Dataset: 99999999}});
+
+
+/*db.collection("Recources").find().then(doc => { 
+    doc.forEach(elem => {
         console.log(elem);
     })
-})
-    
+})*/
+
+/*db.collection("UserRatings").find({email: '999'}).then(elem => { 
+    elem.forEach(stuff => {
+        stuff.ratings.find({movieId: 23}).then((res)=> {
+            console.log(res);
+        }) 
+        
+    })
+}) */
  
+
+
+
+
+
 /*
 all = all.trim();  // final crlf in file
 let lines = all.split("\r\n");
@@ -149,8 +464,8 @@ for(let i = 0; i < 610; i++) {
     console.log(res.ratings.rate);
 });*/ 
 
-let testUser = new UserRatings({email: '999', average: 99, ratings: [{movieId: 30, rate: 50},{movieId: 23, rate: 69},{movieId: 10, rate: 55}]});
-let movierating = {movieId: 44, rate: 21};                                                         //TESTING PURPOSES
+
+                                                        //TESTING PURPOSES
 
 /*db.collection("UserRatings").insert(user, function (err, results) {
     console.log(results)
--- src/app/Components/Class/recommendation/recommendation.ts
+++ src/app/Components/Class/recommendation/recommendation.ts
@@ -1,14 +1,38 @@
 import DatasetFunction from '../../../../assets/detaset';
 import { HttpClient } from '@angular/common/http';
+import { ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser';
 
 export abstract class Recommendation {
     static currentUserRatings = [];
-    static userLength = 610;
-    static movieLength = 9744;
+    static userLength = 0;                                   //server user length and movie length
+    static movieLength =0;
     static Matrix = [];
 
 
+    static initLengths(){ 
+         return fetch('http://localhost:3000/get-length').then(response => { 
+            return response.json();})
+    }
+
+
+
+    static initUser(email: string){
+        var result: number[] = new Array(this.movieLength);
+    
+       return fetch('http://localhost:3000/user/?email=' + email).then(res => { 
+                      return res.json();}).then(user =>{ 
+                        result[0] = user.email;
+                        result[1] = user.average;
+                        var ratings = this.adjustMovieArray(user.ratings);//read all ratings in the correct form
+                        for(var j = 2; j < ratings.length; j++)//fill in the return matrix with rating vectors one at a time
+                        result[j] = ratings[j-2];//the ratings array is shifted by two because it does not have email and average
+                    return result;
+                    })
+    
+    }
     static adjustMovieArray(arr): number[]{
+        
+
         var result: number[] = new Array(this.movieLength - 2);//initialize an array for storing only movie ratings
          for(var i = 0; i < result.length; i++)//fill the array with zeros
             result[i] = 0;
@@ -16,9 +40,10 @@ export abstract class Recommendation {
             result[arr[i].movieId] = arr[i].rate;
         }
         return result;
+  
     }    
 
-    static createMatrix(){
+    static createMatrix(email:string){
         
         var position: number = 0;
         const ratingMatrix = [];
@@ -31,6 +56,7 @@ export abstract class Recommendation {
     .then(response => {
        return response.json();}).then((emails) => {   
           emails.forEach(element => {
+             if(element != email)   {
              fetch('http://localhost:3000/user/?email=' + element).then(res => { 
                return res.json();}).then((user) => { 
                    ratingMatrix[position][0] = user.email;
@@ -44,7 +70,8 @@ export abstract class Recommendation {
                        ratingMatrix[position][j] = ratings[j-2];           //the ratings array is shifted by two because it does not have email and average
                    
                        position++;
-            }) 
+            })
+            } 
           })
           return ratingMatrix;
        })
@@ -303,7 +330,7 @@ export abstract class Recommendation {
 
         this.sort(array, index);
 
-        for (let i = 0; i < k; i++) {
+        for (let i = 0; i < k; i++) {``
             neighborsID[i] = index[i];
         }
 
@@ -312,50 +339,63 @@ export abstract class Recommendation {
 
 
 
-    static recommend(ratingsArray: number[]): number[] {
+    static recommend(email:string) {
+        var bestMovies = [];
+        this.initLengths().then((res) => { 
+            this.userLength = Number(res[1]);
+            this.movieLength = Number(res[0]) + 2;
 
+            //this.currentUserRatings = ratingsArray;
+            this.createMatrix(email).then(res => { 
+                 let ourMatrix = res;
 
-        this.currentUserRatings = ratingsArray;
-        const ourMatrix = this.createMatrix();
+                this.initUser(email).then((res) => { 
+                    this.currentUserRatings = res;
+              
         
-        const weights: number[] = new Array(this.userLength);
-        const predictedUserScores = new Array(this.movieLength - 2);
+            const weights: number[] = new Array(this.userLength);
+            const predictedUserScores = new Array(this.movieLength - 2);
 
-        for (let i = 0; i < this.userLength; i++) {
-            weights[i] = this.pearson_similarity(ourMatrix[i], this.currentUserRatings);
-        }
-
-        const neighbors = this.nearestKNeighbors(weights, 20);
-
-
-        const neighborWeights: number[] = new Array(20);
-        for (let i = 0; i < neighborWeights.length; i++) {
-
-            neighborWeights[i] = weights[neighbors[i]];
-
-        }
-
-        const neighboursRating = new Array(20);
-        for (let i = 0; i < neighborWeights.length; i++) {
-            neighboursRating[i] = ourMatrix[neighbors[i]];
-
-        }
-        const tneighbor = this.transpose(neighboursRating);
-
-        for (let i = 2; i < this.movieLength; i++) {
-            if (this.currentUserRatings[i] == 0) {
-                predictedUserScores[i - 2] = this.scorePrediction(tneighbor[i], neighborWeights, tneighbor[1], this.currentUserRatings[1]);
-                
+            for (let i = 0; i < this.userLength; i++) {
+                weights[i] = this.pearson_similarity(ourMatrix[i], this.currentUserRatings);
             }
 
-            else {
-                predictedUserScores[i - 2] = 0;
+            const neighbors = this.nearestKNeighbors(weights, 20);
 
+
+            const neighborWeights: number[] = new Array(20);
+            for (let i = 0; i < neighborWeights.length; i++) {
+                neighborWeights[i] = weights[neighbors[i]];
             }
+
+            const neighboursRating = new Array(20);
+            for (let i = 0; i < neighborWeights.length; i++) {
+                neighboursRating[i] = ourMatrix[neighbors[i]]; 
+            }
+            
+            const tneighbor = this.transpose(neighboursRating);
+            for (let i = 2; i < this.movieLength; i++) {
+                if (this.currentUserRatings[i] == 0) {
+                    predictedUserScores[i - 2] = this.scorePrediction(tneighbor[i], neighborWeights, tneighbor[1], this.currentUserRatings[1]);
+                }
+                else {
+                    predictedUserScores[i - 2] = 0;
+                }
         }
 
-        const bestMovies = this.nearestKNeighbors(predictedUserScores, 100); // this is the number of recommended movies we want to display in watch me. 
-        return bestMovies;
+        bestMovies = this.nearestKNeighbors(predictedUserScores, 100); // this is the number of recommended movies we want to display in watch me. 
+        this.initTranslationArray().then((res) => { 
+            const translation = res;
+            var recommendedmovies = bestMovies
+            .map(x => translation[x]);
+    
+            console.log(recommendedmovies);
+          this.storeRecommendation(email,recommendedmovies);
+        })
+             });
+        });
+        })
+        
     }
     static transpose(array) {
         const transArray = new Array(this.movieLength);
@@ -372,15 +412,33 @@ export abstract class Recommendation {
         return transArray;
     }
 
+   static initTranslationArray(){
+
+   
+        return fetch('http://localhost:3000/translation').then((response) => {
+         return response.json();});
+       }
+    static storeRecommendation(email: string, recommendations: number[]) {
+        fetch('http://localhost:3000/store-recommendation', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({email, ratings: recommendations})
+        }).then((e) => {
+          console.log(e);
+        });
+      }
 
     static async initMatrix() { 
-        
+     /*   
         console.log("wat");
         await this.createMatrix().then((matrii) => { 
           this.Matrix = matrii;
           
         });
-        console.log(this.Matrix);
+        */
         
       }
 
